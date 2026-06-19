2ec96b5644383d1b76b120d9befc801c1dfb636d
kekkofs75 <kekko.fs75@gmail.com>
2026-06-19 16:46:32 +0200

    fix riassunto tag team

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
    Modifica CACHE_VERSION per forzare un aggiornamento.
    ═══════════════════════════════════════════════════════════ */
 
-const CACHE_VERSION = 'peg-v40';
+const CACHE_VERSION = 'peg-v41';
 const CACHE_NAME    = `prepara-gioca-${CACHE_VERSION}`;
 
 /* File core da precachare al momento dell'install */
